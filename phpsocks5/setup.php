5549be1db8363a6d36d16b9a082f3e1c568b23c8
sonyandy123 <32289997+sonyandy123@users.noreply.github.com>
2018-01-25 10:00:37 +0800

    Add files via upload

---
+++ phpsocks5/setup.php
@@ -0,0 +1,370 @@
+<?php
+$dbhost = 'mariadb';
+$dbport = '3306';
+$dbuser = 'phpsocks5';
+$dbpass = 'poiu0987';
+$dbname = 'phpsocks5';
+
+$secretkey = 'EI2efISbZuAQLyyY1kFxAOoqS4dofEXKZaYrSA2aBDdHuwziWzW5qY8xczTblE39NbRQC8VF6bpq7iLFGpDXCYXR4aZDvmteypjTQQV1BnQsOf1RtxYL4DCefuA7dsMfH42oWgp424YcnDIIE8pWMAkrA8kwh2VMDjnOPZNUf6a1nOzYEzKzB5v7a6EBnF5tNDyfIdQmTPrvRU5yI7bkJBSIjIWF7GmFrfoWeTBWPk2pcvXpmQsInyAO5yHeUGXORqyFc1RHRwJbnVHdhgpHxEL8mWwRQWMguUqnSCdsGrCgklZCpNUOVGNk3A4zmWAQodAPPn3kGmNNlkXptmUYvNZ4jnlvKI0nDMrIhYNhemrRvp2s8IXJFaOs49YNsuW0vo5kqZMp1cOxmwdS227WtgjNqEJRQGYcsHdPCrwhvf7T81wVKgSEpPW5fudscDADgoTlkFMovGTFDdFyIXlq2xjubqL8apkEfne07UiGkBIxzyCLGoeYBjJQtaDXpvVBqyI0vdkkkPKSnxySMJszTKiQ934nRsAwQe2bKdlfZ9nwAhWOVEJ7skSob8zfhhnPQ8YDjBveA9IGzAArkABSZvzbhWvzgK9PVSQVvtkBxB1Xp8NZgXSNVToFvyPrIAyn9JFVKZqbGn8zTMr6HAdsRatGscbAWukq4IuuXvuF2ON4wyqrt2gS2QCwcfZFrXaroLGX8NxvqQ0lR9gHURHEJShWDFqu6iU7dSuBkArdxy0Eo9bRAJEOIHLRYa8cGFhthbIcIl4PuWMd5ImjxkDtwdYwTNQ0svQxO14tYQbb9vpENcgumwmHKWmr2UDYPao50ahtz6IK0jnlyLeJmASv1rOh8m3c3P04cZF3iXJ4tfQzgxZNkBNPn5mxHHJceSkiEbUUtdujhR3bSI8Rykq9YDmhjcEUySvL8oIEB5NibjtFF1xfCXOPR2EMlbBU9taIZO9jaBLVvSrx6HHLs9xKJftmI9C4UJ5wdE4iUzDCLMnhVchPkUMRPDXdfUlAMqWhMrcBwhzbOgpG2CG9WK1HDoMqkPoeT0TV9kIhCCBH7E3GAAkHw8vKjsYYF453LkpNQf7uX0A72hPdQNG63Bo7kWD2JmWX5o5JSRSNHKYyL4r7gJ15fqiigOT9JCA8HDiRZu8Y4R1YNWl8qEQcCCaLLYoqvi9EH4qOS6k9TJJiHKaubdLxYsKGDrkxW5xCjfFtsjHZjUQqyChnfvhZZ7w0oHfKeMwXDkSiYvwmo7AYMoeu0IIWM66iLmy6u09uE3HjE5DIgRrcmUVtWzoLAZksiFQF1GLlY8OM9DIWtJOEfLW9teBzMc99ajnTBtDzXPL7cHZDM5OKZTz3e375hm4cqwkBYx0szTicxyVJNmlOITTdaxuPpOD5IzsuC2DO4bXYbfmzyKTLNGDOJPXj4A1tXMCl5edcbd1DUbYsMKSkm3fPA0uiOzYe9BhKNmhSQxS0iM6hrpxDreYhGjSHj2jZ9gCj9tvfFnNQOYdwnRwBVxtnsmzzQ7rjYzXu23i2br45pnPzujukhkWa3let5DFrQ0tCSFRr02BDsw4Lrptx5hcTT7JWUND8wlOBr9cgYWgBbTCCzGcB2eheWq75vkMRus2jIuXGY34R9GDCP77qn6lkhMH3huvkKDoHHKynYWletWPXHNNLt3ddAi7pqTbhO1zVrKHOakq0o0L7LXRHxxhZf0OrCxSfc1q1uTZmaYww7SOElSVqX1lWY4ulbn8nF0VXkyKJCzPxCKcG5XbYNgAmEOExTgMR0Vz6ulABi1pmhlnibyBBsUGnhGpKXDcqurijHuqrLBbivisXsUCujy4HRIJFqe94TL1DPIYsbjxJTYSgW5Msv34XXy3t2497Bl291SRkAu8B8dfpHHZlwRU5xfyepcEL67uHvnMCnWjD2JIPFLikBXeYGmbxpB99nf343tziWhWt0950V1flfJadODNGW8lrW5oVlD08CxGwFPIlsKVu1i0amxCJWUHue0OCT1ekDYyE9gfEvg26c3Pll6Ia5AeEIjsdQSYQTwIpXtLPJIW46uWS6zC8tgMwKGhonxgAagGVyWL0LlfW01v2tb7Y3qMFOVB10wglQOAEP27zhtvangwEhJTDNwGaSkUvIeOon8jA6bKOWtuWy9pmqjgnq2r2SRgqaLawQtcjk43HjiEfZUAI1wI3PWn2A96OTe3DFS3xp818VbPZuaVJEuEm20XCNn2EfEoyoOnEIpFIA3BXQLHen9L5tOdZOCYASfki3NH2SCto6rxBzJw3keHuvA7yalbSbfFKIl5A3LJTAWcXGPMxT5VlZzGgORSMNKLdONd7kUQ2BnS7TuYhVBZjnaXNT3G1uvy1MY4eeZIrkeSOIlSjfTnjPloE6iyRmiPSvTVjJtzaEJnV85CMP7v4RnqPqcPtm8BkgcPcl3dF7eYDUxRVTDK33vzcWygO491yCkJG0CnWaok3iQ1nf5BBSJ99IHlbQw7AXkkwZT1IC1amxjvxz6AGC20v9pSqsklO25P9OjkP0mu8IxMQ4S7yjPUyaNNRU6xsBJbh3SJjr90fJeD8dREb3fS3QERHT8bYzpx541MQxSZ1fvfeVtI2qhkc7QQaUGfjJ0KCX6QUX4xM9j8VbkG6d8Gw3Dt6C4n5iOx65QjY4ZfzoLPqK62vPmaTInEH9U1tNNxFyhIsR36QQA2DCJcpH4XdSO8up7oVMvzlRpJhZqbSEizghRIEZyIbuL6xvyN25dADQOii6GciquaBMxIx05UYBkQZ0IBQ21FKNIytbnGDRdd88bYkKulzc5FRGjiWl86GRnHUhR2TGPnGyemzwUOVJFuMekVh8HMsqgmNjKqWIVIjkFli0CLdHpf38DQTlANSThHXVZDUABnSI9vsgrPzrUSCMMEYo5NVsKzrJOnEQAZhndCrb4kkOq7pNvH1sZuwHfdCGT9IwVsxNgxBjnYF9KeloUfnL8aR3irbpci4eWc3zMQaG70vUXxb7BMPjMt5KkkvcX0IR6HRTaEDxTZTIcccAYruYBVFq7hYQ2DgPE2qxwBBcpKFa55D8gsACRdfB7TTXKtuGkB7QyJGtNqvVmzPUgSGEgku2FbZEfvn4NnHcVEvEJ1SU6ogNCAGoDUxxgx7eFrkCxav6OidCvM9cWl19QfCVPs5AvWZlEKRU37ocPOFzxOAKHqHEDWGAGlzhoafiDnmhdAhkSvu79T5MRGhCQ9IH9RBri1cMld4G8PRhxIuV4fur34m2bAUKxfbmyuk2JHTAjHKSXH4lHzBrj0GowuPcKlWLdKAcHngEpAi8zO8aXtPvHbXQI9AED7fJMUuoQYvSsmrXPqddVwg3obAigCuChrZji59hSI2qMq07crmVUperrXimr9Z42mEYYrmYuN3F5yjwsJpyYXe0ygmWJRncySyWlQAqGAmbgoaBmRUXus6qO2NYHZDmGpdKCODYGwnQNVsdTyjBO5DO3oSCEAsuUjpxwidjcmbQ2Eoc0PpKNxoPr2hoUGzJ8xiPfFUEkq8WQGs35aTDptdt7NgR7outsMmTTbxq2qO3hyKpySTGeSbiIJ5W6WKB4HzxhFQX5sO9EJARdDKzF6ZY2UK5gmlafKA2FogyNHOVaU8nEaNuM6C4MLKz40JnBBgt1kXO51IJom8LiIvpAk9IDvfWGCZa2tEbY6aXJ6MRvmnSq1rOcrRI78nHHcuo3E2I5eD4JLBulkKxGMLn0UAAEyf8TvtZMSDWeCw2BNmZuGc51AqrltM6ZEYeGM4hKANseDseis851BZXB5MBdfDuUN3bwOsZxWXwTdbOW4r5LhjnnQyowNPYPcWsrMuzMgHKUy7JMV8fSp7o37Wr86qEqHVRraNTfW0AGgCUm0t2K2KnhAiakWXPcVfkqerkwSkg0hrtzwGWX8rOk1JfM3lJK3oHitCPams8GKEQwjFksQkwMbehyOTa8P9PXAozryMsvGVAjak317QB5AOud7wAiEyOuy88gQ4nM2IzCTdEc1i3DjVGBoiXXuPnbbI5eQQyvW7JmPGVYLMnWbbeQ7k2HTTzRlCRPGJVfnD8cxAcQwQL8BMRNLqffeR2XLd79i9gIGZKywyQNiHul3ICx3DqMMKiXLnaSgJPKxCEmRbU3Bgq6Domi7YiICpsRL3LHDllc8gOX4j4wV3kTjMVwv4V5QHzKopfKbtw28xcQTTLpVAmpU5h4mwCny10d4taZpgMOSNIe3HobBuOsIRV9jxTywxRUCq53J77j8vu7wB0IJT0U7GplPUom7NcKNlcvmjViFrrDKHVHMdIRk2c5KZ1cv1KtPGrmSY0KrOTxsH0rdLOIUMQCLOolTOdFRfcCZCPt1mKOVcw2ffcYKk5hLrjNzLvI6U8VuH9FCcm9NV0TwmEAAZnVDbOnPqFKeAWtr2zClMlLCSEqgNd7QNOy46qxF8uxb1VebBtCw4qhaz9G7DfRpKkCj3DBkwGKTXQnR4ANpXUrzSD9n3CIU2gcO7h8F7pLnjSoa2shjVd4VPZCLg3TCVtrfkf4lRXWRx4h23ORnBSzIstcDF33xQClhQ7LsgQj5snKndOsr8QqxAij0vQJrd8ljzL7utMN1dAwIuvGSDdlKlmgkkLw6scUkNqPE28rCMzy33o2HrINSWmUbdqNHu2YTTTDtiq9Uvp8eqIEfjWlt7LImrvdyaTdbKu9YLPgCrfeufxIUKQR9i8teXq4aVFrDUH3VOPXsayqGVmvEDOqRzPWBXpgLhdLYl449xAsKwuyYQZLN8bsiCfqZYqTcdt1v2NpWb4RsqlCs9ejtfrnqjZEBUh7WWblzVCyFRBcvIkynxeyCxaIDjFLd0Txhk1h92EscPzUibxXmnkZ9mBvO4blIP56Mf0kKqgeyrWDMoZkshvrSmUf7Y8Bqi7udewVuQXMJOil6XM7fOlUE3gndJpXF3yc5EzNvgrXCsuitnR7Q8B6owD14YMbqDBKYhflaMbtoojT7LDjZwiKv1CqJT';
+$debuginfo = True;
+$prefix = 'ZjpU56EqYlvgPMRtAbd6bGMrlxyOr7';
+$postfix = 'uR7uppcUtwL0rKoEymoX5qaMmgKQbI';
+
+$dbprefix = 'phpsocks5_';
+$invstep = 100000;
+$invmax = 3000000;
+
+$content_type = 'image/png';
+$sesscookiekey = 'PHPSESSID';
+$version = "03";
+
+function phpsocks5_encrypt($datastr)
+{
+	global $secretkey;
+	$encrypted = '';
+	for($i = 0; $i < strlen($datastr); $i++)
+		$encrypted .= chr(ord($datastr[$i]) ^ ord($secretkey[$i % strlen($secretkey)]));
+	return $encrypted;
+}
+
+function phpsocks5_decrypt($datastr)
+{
+	return phpsocks5_encrypt($datastr);
+}
+
+function phpsocks5_tohex($datastr)
+{
+	global $debuginfo;
+	if(!$debuginfo)
+		return '';
+	$hexstr = bin2hex($datastr);
+	$hexstr .= '(';
+	for($i = 0; $i < strlen($datastr); $i++)
+	{
+		if($datastr[$i] < ' ' || $datastr[$i] > '~')
+			$hexstr .= '.';
+		else
+			$hexstr .= $datastr[$i];
+	}
+	$hexstr .= ')';
+	return $hexstr;
+}
+
+function phpsocks5_log($message)
+{
+	global $dbprefix;
+	global $debuginfo;
+	if(!$debuginfo)
+		return;
+	error_log(date(DATE_RFC1123) . "\t" . $message . "\n", 3, $dbprefix . "log.log");
+}
+
+function phpsocks5_http_500($errmsg)
+{
+	header('HTTP/1.1 500');
+	echo phpsocks5_encrypt(date(DATE_RFC1123) . "\t" . $errmsg);
+	mysql_close();
+	phpsocks5_log("http_500_" . $errmsg);
+	exit;
+}
+
+function phpsocks5_usleep($usec)
+{
+	global $dbhost;
+	global $dbport;
+	global $dbuser;
+	global $dbpass;
+	global $dbname;
+	phpsocks5_log("sleep process 1");
+	mysql_close();
+	phpsocks5_log("sleep process 2");
+	usleep($usec);
+	phpsocks5_log("sleep process 3");
+	if(!mysql_pconnect("$dbhost:$dbport", $dbuser, $dbpass))
+		phpsocks5_http_500('mysql_pconnect error');
+	if(!mysql_select_db($dbname))
+		phpsocks5_http_500('mysql_select_db error');
+	if(!mysql_query('SET AUTOCOMMIT=1'))
+		phpsocks5_http_500('mysql_query SET AUTOCOMMIT=1 error');
+	phpsocks5_log("sleep process 4");
+}
+
+phpsocks5_log("process 1");
+
+$phpsid = mysql_escape_string($_COOKIE[$sesscookiekey]);
+
+phpsocks5_log("process 2 $phpsid");
+
+set_time_limit(30);
+
+phpsocks5_log("process 3 $phpsid");
+
+if(!mysql_pconnect("$dbhost:$dbport", $dbuser, $dbpass))
+	phpsocks5_http_500('mysql_pconnect error');
+if(!mysql_select_db($dbname))
+	phpsocks5_http_500('mysql_select_db error');
+if(!mysql_query('SET AUTOCOMMIT=1'))
+	phpsocks5_http_500('mysql_query SET AUTOCOMMIT=1 error');
+
+phpsocks5_log("process 4 $phpsid");
+
+$postdata = file_get_contents("php://input");
+
+phpsocks5_log("process 5 $phpsid before decrypt postdata: " . phpsocks5_tohex($postdata));
+
+$postdata = phpsocks5_decrypt($postdata);
+
+phpsocks5_log("process 6 $phpsid after decrypt postdata: " . phpsocks5_tohex($postdata));
+
+if(!$postdata)
+{
+	phpsocks5_log("create table process");
+	$dbversion = 0;
+	if(!mysql_query("SELECT * FROM ${dbprefix}conning"))
+		$dbversion = 0;
+	else
+	{
+		$rslt = mysql_query("SELECT * FROM ${dbprefix}dbversion");
+		if(!$rslt)
+			$dbversion = 1;
+		else
+		{
+			$row = mysql_fetch_row($rslt);
+			if(!$row)
+			{
+				if($debuginfo)
+					echo 'create table process fetch dbversion row error.';
+				mysql_close();
+				exit;
+			}
+			$dbversion = $row[0];
+		} 
+	}
+	if($dbversion == 0)
+	{
+		if(!mysql_query("CREATE TABLE ${dbprefix}conning (  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,  sid VARCHAR(200) NOT NULL,  host VARCHAR(512) NOT NULL,  port INTEGER NOT NULL,  PRIMARY KEY (id))"))
+		{
+			if($debuginfo)
+				echo 'Create table 1 error.';
+			mysql_close();
+			exit;
+		}
+		if(!mysql_query("CREATE TABLE ${dbprefix}sending (  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,  sid VARCHAR(200) NOT NULL,  cnt VARCHAR(8192) NOT NULL,  PRIMARY KEY (id))"))
+		{
+			if($debuginfo)
+				echo 'Create table 2 error.';
+			mysql_close();
+			exit;
+		}
+		if(!mysql_query("CREATE TABLE ${dbprefix}recving (  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,  sid VARCHAR(200) NOT NULL,  cnt VARCHAR(8192) NOT NULL,  PRIMARY KEY (id))"))
+		{
+			if($debuginfo)
+				echo 'Create table 3 error.';
+			mysql_close();
+			exit;
+		}
+		$dbversion = 1;
+	}
+	if($dbversion == 1)
+	{
+		if(!mysql_query("ALTER TABLE ${dbprefix}sending CHANGE COLUMN cnt cnt MEDIUMTEXT NOT NULL  ;"))
+		{
+			if($debuginfo)
+				echo 'Alter table 2 error.';
+			mysql_close();
+			exit;
+		}
+		if(!mysql_query("ALTER TABLE ${dbprefix}recving CHANGE COLUMN cnt cnt MEDIUMTEXT NOT NULL  ;"))
+		{
+			if($debuginfo)
+				echo 'Alter table 3 error.';
+			mysql_close();
+			exit;
+		}
+		if(!mysql_query("CREATE TABLE ${dbprefix}dbversion (  dbversion INT NOT NULL )"))
+		{
+			if($debuginfo)
+				echo 'Create table 4 error.';
+			mysql_close();
+			exit;
+		}
+		if(!mysql_query("INSERT INTO ${dbprefix}dbversion VALUES (2)"))
+		{
+			if($debuginfo)
+				echo 'Init table 4 error.';
+			mysql_close();
+			exit;
+		}
+		$dbversion = 2;
+	}
+	mysql_close();
+	if($debuginfo)
+		echo 'Create tables successfully.';
+	exit;
+}
+
+header("Content-Type: $content_type");
+
+if($postdata[0] != $version[0] || $postdata[1] != $version[1])
+	phpsocks5_http_500('version not match');
+
+phpsocks5_log("process 7");
+
+if($postdata[2] == "1")
+{
+	phpsocks5_log("connect process 1");
+	if(!session_start())
+		phpsocks5_http_500('session_start error');
+	phpsocks5_log("connect process 2");
+	$host = mysql_escape_string(strtok(substr($postdata, 3), ':'));
+	$port = mysql_escape_string(strtok(':'));
+	$phpsid = mysql_escape_string(session_id());
+	phpsocks5_log("connect process 3 $phpsid");
+	mysql_query("DELETE FROM ${dbprefix}conning WHERE sid = '" . $phpsid . "'");
+	mysql_query("DELETE FROM ${dbprefix}sending WHERE sid = '" . $phpsid . "'");
+	mysql_query("DELETE FROM ${dbprefix}recving WHERE sid = '" . $phpsid . "'");
+	phpsocks5_log("connect process 4 $phpsid");
+	if(!mysql_query("INSERT INTO ${dbprefix}conning (sid, host, port) VALUES ('" . session_id() . "', '$host', '$port')"))
+		phpsocks5_http_500('mysql_query INSERT error');
+	phpsocks5_log("connect process 5 $phpsid");
+}
+elseif($postdata[2] == "2")
+{
+	phpsocks5_log("background process 1 $phpsid");
+	$inv = 0;
+	$rslt = mysql_query("SELECT id, host, port FROM ${dbprefix}conning WHERE sid = '" . $phpsid . "'");
+	if(!$rslt)
+		phpsocks5_http_500('mysql_query SELECT error');
+	phpsocks5_log("background process 2 $phpsid");
+	$row = mysql_fetch_row($rslt);
+	phpsocks5_usleep(0);
+	if(!$row)
+		phpsocks5_http_500('mysql_fetch_row error');
+	phpsocks5_log("background process 3 $phpsid");
+	$rmtskt = fsockopen($row[1], $row[2]);
+	phpsocks5_log("background process 4 $phpsid");
+	if(!$rmtskt)
+	{
+		mysql_query("DELETE FROM ${dbprefix}conning WHERE id = $row[0]");
+		phpsocks5_http_500('fsockopen error');
+	}
+	phpsocks5_log("background process 5 $phpsid");
+	if(!stream_set_blocking($rmtskt, 0))
+		phpsocks5_http_500('stream_set_blocking error');
+	phpsocks5_log("background process 6 $phpsid");
+	while(true)
+	{
+		$noop = true;
+		phpsocks5_log("background process 7 $phpsid");
+		if(feof($rmtskt))
+			phpsocks5_http_500('feof');
+		phpsocks5_log("background process 8 $phpsid");
+		$cnt = fread($rmtskt, 1048576);
+		phpsocks5_log("background process 9 $phpsid");
+		if($cnt)
+		{
+			phpsocks5_log("background process 10 $phpsid fread: " . phpsocks5_tohex($cnt));
+			if(!mysql_query("INSERT INTO ${dbprefix}recving (sid, cnt) VALUES ('" . $phpsid . "', '" . base64_encode($cnt) . "')"))
+				phpsocks5_http_500('mysql_query INSERT error');
+			phpsocks5_usleep(0);
+			phpsocks5_log("background process 11 $phpsid");
+			$noop = false;
+		}
+		phpsocks5_log("background process 12 $phpsid");
+		phpsocks5_usleep($inv);
+		phpsocks5_log("background process 13 $phpsid");
+		$rslt = mysql_query("SELECT id, cnt FROM ${dbprefix}sending WHERE sid = '" . $phpsid . "' ORDER BY id ASC LIMIT 1");
+		if(!$rslt)
+			phpsocks5_http_500('mysql_query SELECT error');
+		$row = mysql_fetch_row($rslt);
+		phpsocks5_usleep(0);
+		phpsocks5_log("background process 14 $phpsid");
+		if($row)
+		{
+			$noop = false;
+			phpsocks5_log("background process 15 $phpsid");
+			mysql_query("DELETE FROM ${dbprefix}sending WHERE id = $row[0]");
+			phpsocks5_usleep(0);
+			phpsocks5_log("background process 16 $phpsid");
+			if(!$row[1])
+				phpsocks5_http_500('break');
+			$cnt = base64_decode($row[1]);
+			phpsocks5_log("background process 17 $phpsid fwrite: " . phpsocks5_tohex($cnt));
+			if(!fwrite($rmtskt, $cnt))
+				phpsocks5_http_500('fwrite error');
+			phpsocks5_log("background process 18 $phpsid");
+		}
+		if($noop)
+		{
+			phpsocks5_log("background process 19 $phpsid");
+			$inv += $invstep;
+			if($inv > $invmax)
+				$inv = $invmax;
+		}
+		else
+		{
+			phpsocks5_log("background process 20 $phpsid");
+			set_time_limit(30);
+			phpsocks5_log("background process 21 $phpsid");
+			$inv = 0;
+		}
+		phpsocks5_usleep($inv);
+		phpsocks5_log("background process 22 $phpsid");
+	}
+}
+elseif($postdata[2] == "3")
+{
+	phpsocks5_log("send process 1 $phpsid");
+	if(!mysql_query("INSERT INTO ${dbprefix}sending (sid, cnt) VALUES ('" . $phpsid . "', '" . base64_encode(substr($postdata, 3)) . "')"))
+		phpsocks5_http_500('mysql_query INSERT INTO error');
+}
+elseif($postdata[2] == "4")
+{
+	phpsocks5_log("receive process 1 $phpsid");
+	$inv = 0;
+	while(true)
+	{
+		phpsocks5_log("receive process 2 $phpsid");
+		$rslt = mysql_query("SELECT id, cnt FROM ${dbprefix}recving WHERE sid = '" . $phpsid . "' ORDER BY id ASC LIMIT 1");
+		if(!$rslt)
+			phpsocks5_http_500('mysql_query SELECT error');
+		phpsocks5_log("receive process 3 $phpsid");
+		$row = mysql_fetch_row($rslt);
+		phpsocks5_usleep(0);
+		if($row)
+		{
+			phpsocks5_log("receive process 4 $phpsid");
+			mysql_query("DELETE FROM ${dbprefix}recving WHERE id = $row[0]");
+			phpsocks5_usleep(0);
+			if($row[1])
+			{
+				$cnt = base64_decode($row[1]);
+				phpsocks5_log("receive process 5 $phpsid echo: " . phpsocks5_tohex($cnt));
+				echo $prefix . phpsocks5_encrypt($cnt) . $postfix;
+			}
+			else
+				phpsocks5_http_500('break');
+			phpsocks5_log("receive process 6 $phpsid");
+			break;
+		}
+		phpsocks5_log("receive process 7 $phpsid");
+		$inv += $invstep;
+		if($inv > $invmax)
+			$inv = $invmax;
+		phpsocks5_log("receive process 8 $phpsid");
+		phpsocks5_usleep($inv);
+		phpsocks5_log("receive process 9 $phpsid");
+	}
+}
+elseif($postdata[2] == "5")
+{
+	phpsocks5_log("close process 1 $phpsid");
+	mysql_query("INSERT INTO ${dbprefix}sending (sid, cnt) VALUES ('" . $phpsid . "', '')");
+	mysql_query("INSERT INTO ${dbprefix}recving (sid, cnt) VALUES ('" . $phpsid . "', '')");
+	phpsocks5_log("close process 2 $phpsid");
+}
+
+phpsocks5_log("process 8 $phpsid");
+
+mysql_close();
+?>
